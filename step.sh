bb5c5a4116b03c33e428428d4772d30484cdd4f9
Matt Malone <mattm@apadmi.com>
2020-08-15 13:24:50 +0100

    fix: Dodgy regexp that didn't work as expected

--- step.sh
+++ step.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Adding Brew OpenJDK install to jEnv"
-VERSION = $(jenv add /usr/local/opt/openjdk/ | tail -n 1)
+VERSION=$(jenv add /usr/local/opt/openjdk/ | tail -n 1 | sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g' | sed 's/[^0-9]*//g')
 
 echo "Setting JVM version in jEnv"
 jenv global $VERSION
